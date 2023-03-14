098da74bd32ea272ffe7ea8597814f6d5cfb3360
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2023-03-13 23:24:46 -0500

    first commit

---
+++ app/src/main/java/com/ada/example/helloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ada.example.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello World")
+    }
+}
